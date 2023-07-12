f684ace3d3a6859b3eba5957c7974859bb57c2a1
Brav3Trav3l3r <yashgajbhiye10@gmail.com>
2023-07-12 21:40:10 +0530

    content page

--- src/app/(content)/page.js
+++ src/app/(content)/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="main p-4">
       <div className="p-4">
-        <h1>hello</h1>
+        <h1>Content page</h1>
       </div>
     </div>
   );
